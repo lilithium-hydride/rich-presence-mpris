4c4ce1b986682aa2e06ad989f2c08772458bec33
lilithium-hydride <78992082+lilithium-hydride@users.noreply.github.com>
2022-07-04 23:42:43 -0400

    apply rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,39 @@
 #![feature(iter_intersperse)]
 
-use std::{thread, sync::mpsc, time::{
-	Duration,
-	SystemTime,
-	UNIX_EPOCH}, io}
-;
-use std::io::BufRead;
-use urlencoding::encode;
-use tap::tap::*;
-use mpris::{
-	Event, 
-	EventError
-};
 use discord_rich_presence::{
-	DiscordIpc, 
-	DiscordIpcClient, 
-	activity::{
-		Activity, 
-		Assets, 
-		Button, 
-		Timestamps
-	}
+	activity::{Activity, Assets, Button, Timestamps},
+	DiscordIpc, DiscordIpcClient,
 };
+use mpris::{Event, EventError};
+use std::io::BufRead;
+use std::{
+	io,
+	sync::mpsc,
+	thread,
+	time::{Duration, SystemTime, UNIX_EPOCH},
+};
+use tap::tap::*;
+use urlencoding::encode;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	
-	let (update_tx, update_rx): (mpsc::Sender<mpris::Event>, mpsc::Receiver<mpris::Event>)
-		= mpsc::channel();
+	let (update_tx, update_rx): (mpsc::Sender<mpris::Event>, mpsc::Receiver<mpris::Event>) =
+		mpsc::channel();
 
 	{
 		let (update_tx, update_rx) = (update_tx.clone(), update_rx);
 		thread::spawn(move || {
 			let mut rpc_client = DiscordIpcClient::new("946585878741024789")
 				.expect("Failed to initialize Discord RPC client");
-			
+
 			loop {
 				match rpc_client.connect() {
-					Ok(_) => { 
+					Ok(_) => {
 						eprintln!("RPC connected");
 						break;
 					}
 					Err(err) => {
 						eprintln!("Failed to connect to Discord. Is it running?\n\tError: {err}");
-						
+
 						// Empty the receiver buffer so it doesn't spam Discord when it opens.
 						for _ in update_rx.try_recv().into_iter() {}
 					}
@@ -61,7 +52,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 			let search_engine_url = "https://www.last.fm/search/tracks?q=";
 
 			// Queue up one loop iteration to send initial RPC data
-			update_tx.send(Event::TrackChanged(Default::default()))
+			update_tx
+				.send(Event::TrackChanged(Default::default()))
 				.expect("Failed to send initial RPC data");
 
 			loop {
@@ -96,27 +88,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 					// that actually uses MPRIS properly for multiple artists.
 					let artists = match metadata.artists() {
 						Some(x) => x,
-						None => vec!["N/A"]
-					}.into_iter()
-					 .intersperse(&", ")
-					 .map(String::from)
-					 .collect::<std::string::String>();
+						None => vec!["N/A"],
+					}
+					.into_iter()
+					.intersperse(&", ")
+					.map(String::from)
+					.collect::<std::string::String>();
 
 					let title = match metadata.title() {
 						Some(x) => x,
-						None => "N/A"
+						None => "N/A",
 					};
-					
-					let album =  match metadata.album_name() { 
+
+					let album = match metadata.album_name() {
 						Some(x) => x,
-						None => title
+						None => title,
 					};
 
 					let line_1 = &(artists.as_str().to_owned() + ": " + &album.to_string());
 					let line_2 = &title;
-					let button_search_url = &format!("{}{}", 
-					                                 search_engine_url, 
-					                                 encode(&format!("{} - {}", artists, title)));
+					let button_search_url = &format!(
+						"{}{}",
+						search_engine_url,
+						encode(&format!("{} - {}", artists, title))
+					);
 					let button_search_text = &format!("Find on {search_engine_name}");
 
 					let payload = Activity::new()
@@ -127,34 +122,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 								SystemTime::now()
 									.duration_since(UNIX_EPOCH)
 									.unwrap()
-									.as_secs() as i64
-							)
-						)
-						.assets(
-							Assets::new()
-								.large_image("cat1")
-								.large_text("rich presence api bad. no album art. here is cat instead.")
-						)
-						.buttons(vec![
-							Button::new(
-								button_search_text,
-								button_search_url,
+									.as_secs() as i64,
 							),
-						]);
-					
+						)
+						.assets(Assets::new().large_image("cat1").large_text(
+							"rich presence api bad. no album art. here is cat instead.",
+						))
+						.buttons(vec![Button::new(button_search_text, button_search_url)]);
+
 					match rpc_client.set_activity(payload.clone()) {
 						Ok(_) => {}
 						Err(err) => {
-							eprintln!("Failed to update Discord status. Is it running?\n\tError: {err}");
+							eprintln!(
+								"Failed to update Discord status. Is it running?\n\tError: {err}"
+							);
 							eprintln!("Attempting to reconnect…");
 							match rpc_client.reconnect() {
 								Ok(_) => {
 									eprintln!("Reconnect succeeded, resending data…");
 									match rpc_client.set_activity(payload) {
 										Ok(_) => {
-											eprintln!("Data successfully resent, connection is normal.")
+											eprintln!(
+												"Data successfully resent, connection is normal."
+											)
 										}
-										Err(err) => eprintln!("Failed to resend.\n\tError: {err}")
+										Err(err) => eprintln!("Failed to resend.\n\tError: {err}"),
 									}
 								}
 								Err(_) => {
@@ -167,8 +159,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 			}
 		});
 	}
-	
-	thread::spawn( move || {
+
+	thread::spawn(move || {
 		let player = mpris::PlayerFinder::new()
 			.expect("D-Bus connection failed")
 			.find_active()
@@ -176,7 +168,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		let player_events = player
 			.events()
 			.expect("Could not start player event stream");
-		
+
 		for event in player_events {
 			match event {
 				Ok(event) => match event {
@@ -185,13 +177,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 					| Event::Stopped
 					| Event::PlayerShutDown
 					| Event::Seeked { .. }
-					| Event::TrackMetadataChanged {..}
+					| Event::TrackMetadataChanged { .. }
 					| Event::TrackChanged(_) => {
-						update_tx.send(event.tap_dbg(|x| eprintln!("{:#?}", x)))
-						         .expect("Failed to send MPRIS event update across threads.");
+						update_tx
+							.send(event.tap_dbg(|x| eprintln!("{:#?}", x)))
+							.expect("Failed to send MPRIS event update across threads.");
 					}
 					_ => {}
-				}
+				},
 				Err(err) => {
 					println!("D-Bus error: {:?}. Aborting.", err);
 					// TODO: what do?
@@ -206,7 +199,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 			break;
 		}
 	}
-	
+
 	//rpc_client.close()?;
 	Ok(())
 }
